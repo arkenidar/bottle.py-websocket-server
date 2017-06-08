47c29eac663b2797ec99770e225df667a8d039da
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-06-08 21:05:03 +0200

    working

---
+++ web_socket_client.py
@@ -0,0 +1,31 @@
+'''
+setup installing the python modules
+sudo -H pip3 install bottle gevent gevent-websocket
+
+setup running the server process
+python3 web_socket_client.py
+'''
+
+from bottle import request, Bottle, abort
+app = Bottle()
+
+@app.route('/websocket')
+def handle_websocket():
+    wsock = request.environ.get('wsgi.websocket')
+    if not wsock:
+        abort(400, 'Expected WebSocket request.')
+
+    while True:
+        try:
+            message = wsock.receive()
+            wsock.send("Your message was: %r" % message)
+        except WebSocketError:
+            break
+
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketError
+from geventwebsocket.handler import WebSocketHandler
+server = WSGIServer(("0.0.0.0", 8080), app,
+                    handler_class=WebSocketHandler)
+server.serve_forever()
+

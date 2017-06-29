3a4ea3ac18f084b35df921bbd2450d835033d690
Dario Cangialosi, Coder <dario.cangialosi@gmail.com>
2017-06-29 10:02:10 +0200

    sends a message to every connected page.

--- web_socket_client.py
+++ web_socket_client.py
@@ -9,25 +9,55 @@ python3 web_socket_client.py
 '''
 
 from bottle import request, Bottle, abort
-app = Bottle()
-
-@app.route('/websocket')
-def handle_websocket():
-    wsock = request.environ.get('wsgi.websocket')
-    if not wsock:
-        abort(400, 'Expected WebSocket request.')
-
-    while True:
-        try:
-            message = wsock.receive()
-            wsock.send("Your message was: %r" % message)
-        except WebSocketError:
-            break
 
 from gevent.pywsgi import WSGIServer
 from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
-server = WSGIServer(("0.0.0.0", 8080), app,
-                    handler_class=WebSocketHandler)
-server.serve_forever()
 
+def main ():
+    ''' Main function. '''
+    app = Bottle()
+
+    class Handler(object):
+        '''
+        Sockets handlers.
+        '''
+        def __init__(self):
+            self.sockets = []
+
+        def handle_websocket(self):
+            '''
+            Handle websockets.
+            '''
+            wsock = request.environ.get('wsgi.websocket')
+            if not wsock:
+                abort(400, 'Expected WebSocket request.')
+
+            self.sockets += [wsock]
+
+            while True:
+                try:
+                    message = wsock.receive()
+                except WebSocketError as wse:
+                    dir(wse)
+                
+                try:
+                    for wsocki in self.sockets:
+                        wsocki.send("Your message was: %r" % message)
+                except WebSocketError as wse:
+                    dir(wse)
+
+    handler = Handler()
+
+    @app.route('/websocket')
+    def handle():
+        ''' utility '''
+        handler.handle_websocket()
+
+
+    server = WSGIServer(("0.0.0.0", 8080), app,
+                        handler_class=WebSocketHandler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
